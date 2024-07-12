6c516de2f54c538428b2d6d84d2e1bd0eb7c8b65
Omar Olmedo Ferrer <omarolmedoferrer@gmail.com>
2024-07-12 18:08:38 +0200

    proves metodes arrays

--- scripts/metodes_arrays.js
+++ scripts/metodes_arrays.js
@@ -2,3 +2,37 @@ console.log("%cMètodes d'arrays", "font-weight: bold");
 
 // * provar mètodes concat(), join(), pop(), push(), shift(), unshift(), flat()
 
+animals = ["🐶", "🐱", "🐭", "🐍", "🐘"]
+colors = ["🔴", "🟠", "🟡", "🟢", "🔵", "🟣"]
+
+console.log(animals, colors);
+
+console.log(animals.concat("🐮", {nombre: "vaca"}));
+console.log(animals.concat(colors)); // une los dos arrays
+console.log([animals, colors]);      // crea un array de arrays
+
+console.log(animals.join());
+console.log(animals.join(" / "));
+
+// pop() elimina el ultimo elemento y lo devuelve
+elefant = animals.pop();
+
+console.log(animals);
+console.log(elefant);
+
+console.log(animals.pop());
+console.log(animals);
+
+// push() añade un elemento y devuelve la nueva length del array
+console.log(animals);
+console.log(animals.push("🐍"));
+console.log(animals);
+
+// shift() y unshift() hacen lo mismo que pop() y push() pero en el principio del array
+
+// flat() une en un único array el array y sus subarrays
+array = [1, 2, [3, 4, 5], [[6, 7], 8]]
+console.log(array.flat())           // "aplana" un nivel
+console.log(array.flat().flat());   // "aplana" dos niveles
+
+console.log(array.flat(2));  // "aplana" hasta la profundidad que le digamos (puede ser Infinity)
