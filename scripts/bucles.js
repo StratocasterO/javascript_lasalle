325f09d5d405db3c9c29d6d75b14964d77b26cdc
Omar Olmedo Ferrer <omarolmedoferrer@gmail.com>
2024-07-19 18:55:12 +0200

    contingut bucles

--- scripts/bucles.js
+++ scripts/bucles.js
@@ -1,1 +1,62 @@
 console.log("%cBucles", "font-weight: bold");
+
+for (let i = 0; i < 10; i++) {  // inicialización del contador, comprobación para seguir, incremento
+    console.log("Hola " + i);
+}
+
+console.log("----------------------");
+
+for (let i = 9; i >= 0; i--) {
+    console.log("Hola " + i);
+}
+
+console.log("----------------------");
+
+alumnos = ["Gonzalo", "Carlos", "Ella", "Arnau", "Mary", "Marc", "Xavi"]
+
+for (let i = 0; i < alumnos.length; i++) {  // ejecuta el bucle tantas veces como elementos tiene el array
+    console.log("Hola, " + alumnos[i]);
+}
+
+console.log("----------------------");
+
+for (const alumno of alumnos) {     // recorrer un array sin indice
+    console.log("Hola, " + alumno);
+    // ! alumnos[alumnos.indexOf(alumno)] = "Omar"         MALA IDEA
+}
+
+for (let i = 0; i < alumnos.length; i++) {  // recorrer un array con indice
+    // const alumno = alumnos[i];
+    // console.log("Hola, " + alumno);
+    alumnos[i] = alumnos[i].toUpperCase()  // modifica los elementos del array
+}
+
+console.log("----------------------");
+
+professor = {
+    nom: "Omar",
+    cognoms: "Olmedo Ferrer",
+    edat: 33,
+    localitat: "Piera"
+}
+
+for (const propietat in professor) {
+    if (Object.hasOwnProperty.call(professor, propietat)) {
+        const element = professor[propietat];
+        console.log(`La propietat ${propietat} del professor té un valor de ${element}`);
+    }
+}
+
+console.log("----------------------");
+
+let i = 0;
+while (i < 10) {
+    console.log("Hola " + i);
+    i++;
+}
+
+console.log("----------------------");
+
+alumnos.forEach(alumno => {
+    console.log("Hola, " + alumno);
+});
