fc51edaaaea5ecbb957c005f6fea6456249d9b3c
Omar Olmedo Ferrer <omarolmedoferrer@gmail.com>
2024-07-19 19:39:52 +0200

    afegits exercicis de repas

---
+++ scripts/repas.js
@@ -0,0 +1,7 @@
+// TODO: recrea la funció parseFloat() de manera que agafi els números encara que hi hagi lletres abans i accepti com a separador decimal els símbols ",", "." i "'". P.e: "hola89'234" ha de tornar "89.234", "43'35adeu" ha de tornar "43.35", "amor45.9odi" ha de tornar "45.9"
+
+// TODO: crea una funció que agafi un string i que torni una lletra aleatoria (sense contar espais i signes de puntuació)
+
+// TODO: crea una funció que agafi un text i li'n separi les paraules (sense signes de puntuació) i les torni en un nou string separades per espais (sense usar arrays)
+
+// TODO: crea una funció que agafi un string i que torni una paraula aleatòria continguda en ella (sense usar arrays)
