f737419cedf77b47d8e933b37490bad1a1aced65
Omar Olmedo Ferrer <omarolmedoferrer@gmail.com>
2024-09-27 17:28:08 +0200

    afegida als apunts prova de firebase

---
+++ scripts/firebase.js
@@ -0,0 +1,29 @@
+    const url = "https://pruebasalle-a1814-default-rtdb.europe-west1.firebasedatabase.app/"
+    const project = "omar-olmedo/"
+    const database = "prueba.json"
+
+    fetch(url + project + database)
+    .then(res => res.json())
+    .then(res => {
+        console.log(res)
+        
+        let idAlumne
+
+        for (const id in res) {
+            if (res[id].nom == "Carlos") {
+                idAlumne = id;
+                break;
+            }
+        }
+
+        console.log("ID de Carlos: " + idAlumne);
+    })
+
+    function postAlumne(nom, edat, pelo) {
+        fetch(url + project + database, {
+            method: 'POST',
+            body: JSON.stringify({nom, edat, pelo})
+        })
+        .then(res => res.json())
+        .then(res => console.log(res))
+    }
