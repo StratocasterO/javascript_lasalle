a13a72589bb7623ef46a8d5ca8892b10ddb4a696
Omar Olmedo Ferrer <omarolmedoferrer@gmail.com>
2024-09-27 18:18:09 +0200

    code simplified

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,29 +1,29 @@
-    const url = "https://pruebasalle-a1814-default-rtdb.europe-west1.firebasedatabase.app/"
-    const project = "omar-olmedo/"
-    const database = "prueba.json"
+const url = "https://pruebasalle-a1814-default-rtdb.europe-west1.firebasedatabase.app/"
+const project = "omar-olmedo/"
+const database = "prueba.json"
 
-    fetch(url + project + database)
+fetch(url + project + database)
     .then(res => res.json())
     .then(res => {
         console.log(res)
-        
-        let idAlumne
 
-        for (const id in res) {
-            if (res[id].nom == "Carlos") {
-                idAlumne = id;
-                break;
-            }
-        }
+        // let idAlumne
 
-        console.log("ID de Carlos: " + idAlumne);
+        // for (const id in res) {
+        //     if (res[id].nom == "Carlos") {
+        //         idAlumne = id;
+        //         break;
+        //     }
+        // }
+
+        // console.log("ID de Carlos: " + idAlumne);
     })
 
-    function postAlumne(nom, edat, pelo) {
-        fetch(url + project + database, {
-            method: 'POST',
-            body: JSON.stringify({nom, edat, pelo})
-        })
+function postAlumne(nom, edat, pelo) {
+    fetch(url + project + database, {
+        method: 'POST',
+        body: JSON.stringify({ nom, edat, pelo })
+    })
         .then(res => res.json())
         .then(res => console.log(res))
-    }
+}
